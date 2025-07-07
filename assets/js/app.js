ac2b1f263d0f651d89f014244aad39ac4867dc1b
Cristian Arias Pineda <ariascristian2207@gmail.com>
2025-07-07 17:27:04 -0600

    add comments of which changes I did to fix the code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
+//Add variable names instead of letters without context
 const $name = document.querySelector('.name');
 const $blog = document.querySelector('.blog');
 const $location = document.querySelector('.location');
@@ -8,8 +9,10 @@ async function displayUser(username) {
   $name.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(response);
+  //await the response and convert into json format in order to access on it.
   const json = await response.json();
   console.log(json);
+  //Assign the text in the labels in html
   $name.textContent = `Name: ${json['name']}`;
   $blog.textContent = `Blog: ${json['blog']}`;
   $location.textContent =` Location: ${json['location']}`;
